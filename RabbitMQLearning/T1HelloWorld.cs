15ea3b391f81a40dc72ce7c833330d7ce4868853
chrisfcarroll <chris.carroll@unforgettable.me.uk>
2014-11-28 22:06:42 +0000

    1. Hello World

---
+++ RabbitMQLearning/T1HelloWorld.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace RabbitMQLearning
+{
+    public class T1HelloWorld
+    {
+        public const string QueueName = "hello";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var connection = new ConnectionFactory { HostName = "localhost" }.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(QueueName, false, false, false, null);
+                DemoSendAndReceiveMessageViaRabbitMQ(channel);
+            }
+        }
+
+        static void DemoSendAndReceiveMessageViaRabbitMQ(IModel channel)
+        {
+            //
+            var sendFiveTimes = 
+                Enumerable
+                    .Range(1, 5)
+                    .Select(i => Task.Factory.StartNew( ()=>{SendHello(channel,QueueName, i);}));
+
+            var receiveAndLog = Task.Factory.StartNew( ()=>{ConsoleLogEachMessageInQueue(channel,QueueName);});
+            //
+            Task.WaitAll(sendFiveTimes.ToArray());
+            Task.WaitAll(receiveAndLog);
+        }
+
+        /// <param name="logger">Defaults to Console.Out</param>
+        /// <param name = "listenUntil">How long to listen for. Defaults to 1 second</param>
+        public static void ConsoleLogEachMessageInQueue(IModel channel, string queueName, TextWriter logger=null, TimeSpan listenUntil= default(TimeSpan))
+        {
+            //
+            logger = logger ?? Console.Out;
+            listenUntil = listenUntil == default(TimeSpan) ? TimeSpan.FromSeconds(1) : listenUntil;
+            var stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
+            do
+            {
+                var result = channel.BasicGet(queueName, true);
+                if (result == null) continue;
+                var message= Encoding.UTF8.GetString(result.Body);
+                logger.WriteLine(message);
+            }while(stopWatch.Elapsed < listenUntil);
+        }
+
+        public static void SendHello(IModel channel, string queueName, int i=0)
+        {
+                var message = "Test send" + i;
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", queueName, null, body);
+                Console.WriteLine(" sent  {0}", message);
+        }
+    }
+}
---
+++ Tests/T1HelloWorldTests.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using RabbitMQ.Client;
+using System.Text;
+using System.IO;
+
+namespace RabbitMQLearning.Tests
+{
+    [TestFixture]
+    public class T1HelloWorldTests
+    {
+        [Test]
+        public void Ensure_Local_RabbitMQ()
+        {
+            try
+            {
+                using (var conn = new ConnectionFactory{HostName = "localhost"}.CreateConnection())
+                {}
+            }
+            catch(IOException e)
+            {
+                Assert.Fail(
+                    "Couldn't create a connection to RabbitMQ on localhost. Is rabbitmq-server running? \nException:\b {0}",
+                    e
+                );
+            }
+        }
+
+
+        [Test]
+        public void Send_should_send_a_non_empty_string()
+        {
+            //A
+            string result="";
+
+            RunWithRabbitLocalChannelAndQueue(
+                "Send_should_send", 
+                (channel,queueName) => 
+                {
+                    //A
+                    T1HelloWorld.SendHello(channel, queueName);
+                    result= ListenForFirstMessage(channel, queueName);
+                }
+            );
+
+            //A
+            Assert.Greater(result.Length, 0);
+        }
+
+        [Test]
+        public void Receive_should_receive_whatever_was_sent()
+        {
+            //Expect
+            string testSend = "Test Send " + Guid.NewGuid();
+
+            using (var writer = new StringWriter())
+            {
+                //A
+                RunWithRabbitLocalChannelAndQueue(
+                    "Receive_should_receive",
+                    (c,q)=>{
+                        c.BasicPublish("",q,null,Encoding.UTF8.GetBytes(testSend));
+                        T1HelloWorld.ConsoleLogEachMessageInQueue(c, q, writer, TimeSpan.FromMilliseconds(250));
+                    }
+                );
+
+                //A
+                Assert.That(writer.ToString(), Contains.Substring(testSend));
+            }
+        }
+
+        void RunWithRabbitLocalChannelAndQueue(string queueName, Action<IModel,string> action)
+        {
+            using (var conn = new ConnectionFactory{HostName = "localhost"}.CreateConnection())
+            using (var channel = CreateLocalChannel(conn, queueName))
+            {
+                action(channel,queueName);
+            }
+        }
+
+        IModel CreateLocalChannel(IConnection connection, string name)
+        {
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(name,false,false,false,null);
+            return channel;
+        }
+
+        string ListenForFirstMessage(IModel channel, string queueName, long timeoutMillis=5000)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            do
+            {
+                var result= channel.BasicGet(queueName, false);
+                if (result==null) continue;
+                var message = Encoding.UTF8.GetString(result.Body);
+                return message;
+            }while(stopWatch.Elapsed.TotalMilliseconds < timeoutMillis);
+
+            throw new TimeoutException(
+                string.Format("Didn't get any messages at all on queue {0} within timeout {1}",queueName,timeoutMillis)
+                );
+        }
+    }
+}
+
